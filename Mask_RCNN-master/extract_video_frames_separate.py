056438c98ce34cefd428b850a40026345177dc73
张玮诚 <3132247600@qq.com>
2025-10-07 16:15:29 +0800

    Update extract_video_frames_separate.py

--- Mask_RCNN-master/extract_video_frames_separate.py
+++ Mask_RCNN-master/extract_video_frames_separate.py
@@ -38,7 +38,7 @@ print(f"检测尺寸：{config.IMAGE_MIN_DIM}~{config.IMAGE_MAX_DIM} | 输出尺
 
 # 2. 模型加载
 MODEL_DIR = "./model_cache"
-COCO_MODEL_PATH = "./mask_rcnn_coco.h5"
+COCO_MODEL_PATH = r"https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5"
 
 if not os.path.exists(COCO_MODEL_PATH):
     raise FileNotFoundError(
@@ -182,5 +182,6 @@ if __name__ == "__main__":
     VIDEO_PATH = r"C:\Users\User\Desktop\Mask_RCNN-master\video.mp4"
 
     extract_video_frames(VIDEO_PATH, output_root="video_output")
+
 
 
